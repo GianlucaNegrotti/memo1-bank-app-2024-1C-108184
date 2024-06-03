511a2101e65ca8bb63cc17af9819e18608e02ed3
GianlucaNegrotti <gianluca@DESKTOP-AAOT8C6.localdomain>
2024-06-02 21:52:53 -0300

    promos

--- src/main/java/com/aninfo/service/TransactionService.java
+++ src/main/java/com/aninfo/service/TransactionService.java
@@ -1,6 +1,6 @@
 package com.aninfo.service;
 import com.aninfo.repository.AccountRepository;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import com.aninfo.model.Account;
 import com.aninfo.model.Transaction;
 import com.aninfo.repository.TransactionRepository;
